7ca4d7ddf71f77eb214080e39e9d59691518dc72
Kierran Kelly <kierran.c.k333@gmail.com>
2022-04-03 12:17:34 +0100

    commit

--- src/frontend/pages/Report/Report.js
+++ src/frontend/pages/Report/Report.js
@@ -1,185 +1,211 @@
-import React, { useState, useEffect, useCallback, component } from "react";
-import Nav from '../../components/Nav/Nav'
-import './Report.css'
-import Layout from '../../components/Layout/Layout'
-import Table from '../../components/Table/Table'
-import StatsBar from '../../components/StatsBar/StatsBar'
+import React, { useState, useEffect } from "react";
+import "./Report.css";
+import Layout from "../../components/Layout/Layout";
+import StatsBar from "../../components/StatsBar/StatsBar";
 import Header from "../../components/Header/Header";
-import config from '../../config.js'
-import { Modal, Button, Dropdown, Row, Col, InputGroup, FormControl, ListGroup } from "react-bootstrap";
+import config from "../../config.js";
+import {
+  Modal,
+  Button,
+  Dropdown,
+  Row,
+  Col,
+  InputGroup,
+  FormControl,
+  ListGroup,
+  Form,
+} from "react-bootstrap";
 import axios from "axios";
+import { DatePicker } from "react-datepicker";
 
 const Dashboard = () => {
   const [countries, setCountries] = useState([]);
   const [query, setQuery] = useState("");
   const [modalShow, setModalShow] = useState(false);
   const [multiSelect, setMultiSelect] = useState(false);
+  const [country1, setCountry1] = useState("");
+  const [country2, setCountry2] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [selected, setSelected] = useState("");
+  const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
-  if (modalShow === true) {
-    console.log("active")
+    if (modalShow === true) {
+      const options = {
+        method: "GET",
+        url: config.URL + "/countries",
+        headers: {
+          "X-RapidAPI-Host": config.HOST,
+          "X-RapidAPI-Key": config.API_KEY,
+        },
+      };
+      axios
+        .request(options)
+        .then((res) => {
+          setCountries(res.data.response);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  }, [modalShow]);
+
+  const viewReports = (e) => {
+    e.preventDefault();
+    modalShow(false);
     const options = {
-      method: 'GET',
+      method: "GET",
       url: config.URL + "/countries",
       headers: {
-        'X-RapidAPI-Host': config.HOST,
-        'X-RapidAPI-Key': config.API_KEY
-      }
-  }
-        axios.request(options).then(res => {
-      console.log(res.data);
-      setCountries(res.data.response);
-      console.log(res.data.response);
-    }).catch(err => {
-      console.error(err);
-    });
-  }
-  }, [modalShow])
-
-  const search = items => {
-    if (!query) {
-      return items;
-    }
-    return items.filter(item => {
-      const country = item.toLowerCase();
-      return country.includes(query);
-    });
+        "X-RapidAPI-Host": config.HOST,
+        "X-RapidAPI-Key": config.API_KEY,
+      },
+    };
+    axios
+      .request(options)
+      .then((res) => {
+        setCountries(res.data.response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   };
 
-  // const searchItems = searchValue => {
-  //   setSearchInput(searchValue);
-  //   APIData.filter(item => {
-  //     return Object.values(item).join("").toLowerCase().includes(searchInput.toLowerCase)
-  //   })
-  // }
-
-  // const filteredData = APIData.filter(item => {
-  //   return Object.values(item).join("").toLowerCase().includes(searchInput.toLowerCase)
-  // })
-
-  useEffect(() => {
-    console.log(countries)
-    countries.map((country, i) => {
-      // <ListGroup.Item as="li" key={i}>{country}</ListGroup.Item>
-      console.log(country)
-  })
-  }, [countries])
+  //check whether user has made valid selections
+  const validSelections = () => {
+    if (!multiSelect) {
+      if (country1 === "" || country1 === "--select country--") return true;
+      return false;
+    }
+    if (
+      country1 === "" ||
+      country1 === "--select country--" ||
+      country2 === "" ||
+      country2 === "--select country--"
+    )
+      return true;
+    return false;
+  };
 
   return (
-      <Layout>
-          <Header 
-          title="Report"
-          />
+    <Layout>
+      <Header title="Report" />
 
-{countries.map((country, i) => {
-              // <ListGroup.Item as="li" key={i}>{country}</ListGroup.Item>
-              console.log({i});
-              <p>{i}</p>
-          })}
-
-<Button variant="warning" onClick={() => setModalShow(true)}>
+      <Button variant="warning" onClick={() => setModalShow(true)}>
         Select Country
       </Button>
 
-<Modal
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-      show={modalShow}
-      id="modal"
+      <Modal
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={modalShow}
+        id="modal"
+      >
+        {/* <Modal.Header closeButton> */}
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Country Selection
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={viewReports}>
+            <Row>
+              <Col>
+                <h3>Country 1</h3>
+                <Form.Group size="sm" className="mb-3">
+                  <Form.Select
+                    className="form-select-sm"
+                    onChange={(e) => setCountry1(e.target.value)}
+                  >
+                    <option selected>--select country--</option>
+                    {countries.map((country, i) => (
+                      <option key={i}>{country}</option>
+                    ))}
+                  </Form.Select>
+                  {/* {validSelections && <p>Please select a country</p>} */}
+                </Form.Group>
+              </Col>
 
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Country Selection
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Row>
-          <Col>
-        <h3>Country 1</h3>
+              {multiSelect && (
+                <Col>
+                  <h3>Country 2</h3>
+                  <Form.Group className="mb-3">
+                    <Form.Select
+                      className="form-select-sm"
+                      onChange={(e) => setCountry2(e.target.value)}
+                    >
+                      <option selected>--select country--</option>
+                      {countries.map((country, i) => (
+                        <option key={i}>{country}</option>
+                      ))}
+                    </Form.Select>
+                  </Form.Group>
+                </Col>
+              )}
 
-        <InputGroup size="sm">
-          <FormControl
-      placeholder="Country"
-      aria-label="Country"
-      aria-describedby="basic-addon1"
-      onChange={e => query(e.target.value)}
-      // onKeyUp={e => {
-      //   let query = e.target.value;
-      //   let suggestions = [];
-      //   suggestions = countries.filter(country => {
-      //     console.log( country.toLocaleLowerCase().startsWith(query.toLocaleLowerCase));
-      //   })
-      // }}
-    />
-        </InputGroup>
-        <div className="autocom-box">
+              <Col>
+                <h3>Date</h3>
+                <InputGroup size="sm" className="input-group-sm">
+                  <FormControl
+                    aria-label="Small"
+                    aria-describedby="inputGroup-sizing-sm"
+                    value={date}
+                  />
+                  <Button
+                    variant="warning"
+                    id="button-addon2"
+                    className="input-btn"
+                  >
+                    <span class="material-icons calendar-icon">
+                      calendar_today
+                    </span>
+                  </Button>
+                </InputGroup>
+              </Col>
 
-        <ListGroup as="ul">
-  <ListGroup.Item as="li">
-    Cras justo odio
-  </ListGroup.Item>
-  <ListGroup.Item as="li">
-    Cras justo odio
-  </ListGroup.Item>
+              <Row>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  <Form.Check
+                    type="checkbox"
+                    label="Multi select"
+                    onChange={() => {
+                      setMultiSelect(!multiSelect);
+                    }}
+                  />
+                </Form.Group>
+              </Row>
 
-          {countries.map((country, i) => {
-                         console.log(country);
-                         <ListGroup as="ul">
-              <ListGroup.Item as="li" key={i}>{country}</ListGroup.Item>
-              </ListGroup>
+              <Row>
+                <Col>
+                  <Button
+                    type="submit"
+                    variant="warning"
+                    size="sm"
+                    disabled={validSelections()}
+                  >
+                    {"View Report" + (multiSelect ? "s" : "")}
+                  </Button>
+                </Col>
+              </Row>
+            </Row>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="danger"
+            size="sm"
+            onClick={() => {
+              setModalShow(false);
+              setMultiSelect(false);
+            }}
+          >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Layout>
+  );
+};
 
-          })}
-
-</ListGroup>
-        </div>
-
-  </Col>
-{multiSelect ?
-  <Col>
-        <h3>Country 2</h3>
-
-        <Dropdown>
-    <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-      Select Country
-    </Dropdown.Toggle>
-
-    <Dropdown.Menu variant="dark">
-      <Dropdown.Item href="#/action-1" active>
-        Action
-      </Dropdown.Item>
-      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-    </Dropdown.Menu>
-  </Dropdown>
-  </Col>
-  :
-  <Col>
-          <h3>Country 2</h3>
-
-  <div className="text-center">
-  <Button variant="secondary" size="sm" onClick={() => setMultiSelect(true)}>
-  <span class="material-icons">
-add
-</span>
-  </Button>
-  </div>
-
-  </Col>
-}
-  </Row>
-
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="danger" size="sm" onClick={() => setModalShow(false)}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-
-      </Layout>
-
-
-  )
-}
-
-export default Dashboard
+export default Dashboard;
