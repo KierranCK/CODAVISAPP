efb7f421827a0aa4c97b41c689fe6ebe95e2426b
Kierran Kelly <kierran.c.k333@gmail.com>
2022-04-22 22:24:38 +0100

    Updates made

--- src/frontend/pages/About/About.js
+++ src/frontend/pages/About/About.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./About.css";
 import Layout from "../../components/Layout/Layout";
 import Header from "../../components/Header/Header";
+import { Button } from "react-bootstrap";
 
 const About = () => {
   return (
@@ -14,16 +15,27 @@ const About = () => {
           To effectively manage the pandemic, it is critical to understand the
           current situation and the data which represents it.
         </p>
-        <h3>The Purpose</h3>
+        <h3>The Purpose of CODAVIS</h3>
         <p className="about-text">
-          This application form the practical element of 
-          The purpose of this application is to develop a web application for the
-          analysis and visualisation of COVID-19 data to provide easy access to
-          data interpretation, which can then be used to make more informed
-          decisions on policy, helping to control the virus and protect all our
-          interests. The CODAVIS web application provides real-time and historic
-          COVID-19 data.
+          This application forms the practical element of my (Kierran Kelly,
+          U1956244) Analysis and Visualisation of COVID-19 Data snyoptic project
+          in part fulfilment of the degree of BSc (Hons) in Digital and
+          Technology Solutions. <br /> <br />
+          The purpose of the CODAVIS is to provide a means for the analysis and
+          visualisation of real-time and historic COVID-19 data to provide easy
+          access to data interpretation, which can then be used to make more
+          informed decisions on policy, helping to control the virus and protect
+          all our interests. <br /> <br />
+          To view the HitHub repositiory for this project, click the button
+          below.
         </p>
+        <Button
+          variant="warning"
+          size="sm"
+          href="http://https://github.com/KierranCK/CODAVISAPP/"
+        >
+          GitHub
+        </Button>
       </div>
     </Layout>
   );
--- src/frontend/pages/Error/Error.js
+++ src/frontend/pages/Error/Error.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import './Error.css'
-import { Row, Col, Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Error.css";
+import { Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
     <>
-        <div className='launchpad-title-pane'>
-        <h1>404</h1>
+      <div className="launchpad-title-pane">
+        <h1>404 Error</h1>
         <p>Oops! Page not Found</p>
 
         <Row>
           <Col>
-          <Link to="/dashboard">
-          <Button variant="warning" size="sm">Dashboard</Button>
-          </Link>
-
+            <Link to="/dashboard">
+              <Button variant="warning" size="sm">
+                Dashboard
+              </Button>
+            </Link>
           </Col>
         </Row>
-
-        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Error
+export default Error;
